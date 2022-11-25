9b4ee8b4f576d16c70216af9c47cc13c5ae1f047
Valère Monseur <dev@vale.re>
2022-11-25 13:27:07 +0100

    migrate to vscode and adapt some coding style using vscode extensions

--- js/formatXML.js
+++ js/formatXML.js
@@ -5,61 +5,65 @@ interesting test case: https://bug226786.bmoattachments.org/attachment.cgi?id=13
 */
 
 function formatXML(xml) {
-    const padding = ' '.repeat(2); // set desired indent size here
-    const reg = /(>)(<)(\/*)/g;
-    let pad = 0;
+  const padding = " ".repeat(2); // set desired indent size here
+  const reg = /(>)(<)(\/*)/g;
+  let pad = 0;
 
-    xml = xml.replace(reg, '$1\r\n$2$3');
-    return xml.split('\r\n').map((node, _) => {
-        let indent = 0;
+  xml = xml.replace(reg, "$1\r\n$2$3");
+  return xml
+    .split("\r\n")
+    .map((node, _) => {
+      let indent = 0;
 
-        if (node.match(/.+<\/\w[^>]*>$/)) {
-            indent = 0;
-        } else if (node.match(/^<\/\w/) && pad > 0) {
-            pad -= 1;
-        } else if (node.match(/^<\w[^>]*[^\/]>.*$/)) {
-            indent = 1;
-        } else {
-            indent = 0;
-        }
+      if (node.match(/.+<\/\w[^>]*>$/)) {
+        indent = 0;
+      } else if (node.match(/^<\/\w/) && pad > 0) {
+        pad -= 1;
+      } else if (node.match(/^<\w[^>]*[^\/]>.*$/)) {
+        indent = 1;
+      } else {
+        indent = 0;
+      }
 
-        pad += indent;
+      pad += indent;
 
-        return padding.repeat(pad - indent) + node;
-    }).join('\r\n');
+      return padding.repeat(pad - indent) + node;
+    })
+    .join("\r\n");
 }
 
 /*
 note: this old version requires JQuery
 
 function formatXML(xml) {
-    var formatted = '';
-    var reg = /(>)(<)(\/*)/g;
-    xml = xml.replace(reg, '$1\r\n$2$3');
-    var pad = 0;
-    jQuery.each(xml.split('\r\n'), function(index, node) {
-        var indent = 0;
-        if (node.match( /.+<\/\w[^>]*>$/ )) {
-            indent = 0;
-        } else if (node.match( /^<\/\w/ )) {
-            if (pad != 0) {
-                pad -= 1;
-            }
-        } else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
-            indent = 1;
-        } else {
-            indent = 0;
-        }
+  var formatted = "";
+  var reg = /(>)(<)(\/*)/g;
+  xml = xml.replace(reg, "$1\r\n$2$3");
+  var pad = 0;
+  jQuery.each(xml.split("\r\n"), function (index, node) {
+    var indent = 0;
+    if (node.match(/.+<\/\w[^>]*>$/)) {
+      indent = 0;
+    } else if (node.match(/^<\/\w/)) {
+      if (pad != 0) {
+        pad -= 1;
+      }
+    } else if (node.match(/^<\w[^>]*[^\/]>.*$/)) {
+      indent = 1;
+    } else {
+      indent = 0;
+    }
 
-        var padding = '';
-        for (var i = 0; i < pad; i++) {
-            padding += '  ';
-        }
+    var padding = "";
+    for (var i = 0; i < pad; i++) {
+      padding += "  ";
+    }
 
-        formatted += padding + node + '\r\n';
-        pad += indent;
-    });
+    formatted += padding + node + "\r\n";
+    pad += indent;
+  });
 
-    return formatted;
+  return formatted;
 }
+
 */
